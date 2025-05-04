b2a1ff84ba68b72d2a647cfc68cc3593c860acda
Sebastian Lech <lechsebastian99@gmail.com>
2025-05-04 22:26:44 +0200

    Set theme to ThemeData.dark()

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Sebastian Lech',
+      theme: ThemeData.dark(),
       home: const HomePage(),
     );
   }
