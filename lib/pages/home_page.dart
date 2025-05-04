567f1a9f1506fb6796619328adcd7ccf1b9337ac
Sebastian Lech <lechsebastian99@gmail.com>
2025-05-04 23:31:31 +0200

    Move NavBarDesktop widget to separate nav_bar_desktop.dart file

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:portfolio/constants/colors.dart';
-import 'package:portfolio/constants/nav_items.dart';
+import 'package:portfolio/widgets/nav_bar_desktop.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,46 +18,8 @@ class _HomePageState extends State<HomePage> {
         scrollDirection: Axis.vertical,
         children: [
           // NAVBAR
-          Container(
-            height: 60,
-            width: double.infinity,
-            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  Colors.transparent,
-                  CustomColor.bgLight1,
-                ],
-              ),
-              borderRadius: BorderRadius.circular(100),
-            ),
-            child: Row(
-              children: [
-                Text(
-                  'SL',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: CustomColor.yellowSecondary),
-                ),
-                Spacer(),
-                for (int i = 0; i < navTitles.length; i++)
-                  TextButton(
-                    onPressed: () {},
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Text(
-                        navTitles[i],
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                            color: CustomColor.whitePrimary),
-                      ),
-                    ),
-                  ),
-              ],
-            ),
-          ), // SKILLS
+          NavBarDesktop(),
+          // SKILLS
           Container(
             color: Colors.blueGrey,
             height: 500,
---
+++ lib/widgets/nav_bar_desktop.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/constants/colors.dart';
+import 'package:portfolio/constants/nav_items.dart';
+
+class NavBarDesktop extends StatelessWidget {
+  const NavBarDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.transparent,
+            CustomColor.bgLight1,
+          ],
+        ),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        children: [
+          Text(
+            'SL',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: CustomColor.yellowSecondary),
+          ),
+          Spacer(),
+          for (int i = 0; i < navTitles.length; i++)
+            TextButton(
+              onPressed: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text(
+                  navTitles[i],
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: CustomColor.whitePrimary),
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
